01b28e1ece09f55253019e88d7887fb33beec214
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-15 23:09:13 -0500

    -More fixes

--- environment/prod/bootstrap.sh
+++ environment/prod/bootstrap.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 bastion-init() {
-  terraform init | tee tfinit.txt 2>&1
-  terraform apply -no-color -auto-approve | tee tfrun.txt 2>&1
+  #terraform init | tee tfinit.txt 2>&1
+  #terraform apply -no-color -auto-approve | tee tfrun.txt 2>&1
 
   export VAULT_ADDR=https://localhost:8200
   export VAULT_TOKEN=$(cat admin.token)
-  vault status
-
   export SUBASTION_KEYFILE=$(pwd)/bastion.key
   export SUBASTION_IP=$(vault read -field=ip subastion/ec2host)
+
   ssh-keyscan -H $SUBASTION_IP >> ~/.ssh/known_hosts
+
 }
 
 bastion-destroy() {
