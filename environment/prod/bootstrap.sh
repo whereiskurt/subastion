1c081f601eff372756bbd5a231b7b7a4fed982e5
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-17 18:33:56 -0500

    -Adding ignore details, renaning func() in bootstrap

--- environment/prod/bootstrap.sh
+++ environment/prod/bootstrap.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-bastion-init() {
-  #terraform init | tee tfinit.txt 2>&1
-  #terraform apply -no-color -auto-approve | tee tfrun.txt 2>&1
+subastion-init() {
+  terraform init | tee subastion.tfinit.log 2>&1
+  terraform apply -no-color -auto-approve | tee subastion.tfrun.log 2>&1
 
   export VAULT_ADDR=https://localhost:8200
   export VAULT_TOKEN=$(cat admin.token)
   export SUBASTION_KEYFILE=$(pwd)/bastion.key
   export SUBASTION_IP=$(vault read -field=ip subastion/ec2host)
 
+  sleep 5 ##Waiting for ssh'd to come-up
   ssh-keyscan -H $SUBASTION_IP >> ~/.ssh/known_hosts
 
 }
 
-bastion-destroy() {
-  terraform destroy -no-color -auto-approve | tee tfrun.txt 2>&1
+subastion-destroy() {
+  terraform destroy -no-color -auto-approve | tee subastion.tfrun.log 2>&1
   docker kill vault
   docker rm vault
   rm -fr ../../docker/vault/volumes/file/*
@@ -26,14 +27,14 @@ bastion-destroy() {
   unset SUBASTION_IP
 }
 
-bastion-ssh () {
+subastion-ssh () {
   vault read -field=pem subastion/ec2host|base64 -d > $SUBASTION_KEYFILE
   chmod 400 $SUBASTION_KEYFILE
   ssh -i $SUBASTION_KEYFILE ubuntu@$SUBASTION_IP
   rm -f $SUBASTION_KEYFILE
 }
 
-export -f bastion-init
-export -f bastion-destroy
-export -f bastion-ssh
+export -f subastion-init
+export -f subastion-destroy
+export -f subastion-ssh
 
