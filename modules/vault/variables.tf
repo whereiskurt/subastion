d904f93d1abcf096aea29c72c7179a463e4ea773
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-18 16:06:20 -0500

    -Typo

--- modules/vault/variables.tf
+++ modules/vault/variables.tf
@@ -1,6 +1,14 @@
 variable "aws_region" {
   type = string
 }
+variable "aws_access_key" {
+  type = string
+  sensitive = true
+}
+variable "aws_secret_key" {
+  type = string
+  sensitive = true
+}
 variable "aws_kms_key_alias" {
   type = string
 }
