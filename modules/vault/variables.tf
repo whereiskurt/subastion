8160cf888980e7fefb55c46a595ad80ff133aadb
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-18 14:48:30 -0500

    -Factoring out the map for aws_config

--- modules/vault/variables.tf
+++ modules/vault/variables.tf
@@ -2,6 +2,25 @@
 #   type = map
 # }
 
+variable "aws_region" {
+  type = string
+}
+variable "aws_access_key" {
+  type = string
+  sensitive = true
+}
+variable "aws_secret_key" {
+  type = string
+  sensitive = true
+}
+variable "aws_kms_key_alias" {
+  type = string
+}
+variable "aws_kms_key_id" {
+  type = string
+  sensitive=true
+}
+
 variable "aws_build_tags" {
   type = map
 }
