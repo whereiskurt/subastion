c844b951b92b3f44b00eeaad9a2f2a479a729666
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-19 11:29:43 -0500

    -Switching access/secret keys to vaultuser

--- modules/awsvault/outputs.tf
+++ modules/awsvault/outputs.tf
@@ -2,12 +2,15 @@ data "template_file" "vault_conf" {
   template = "${file("../../docker/vault/vault_config.tpl")}"
   vars = {  
     region        = "${var.aws_region}"
-    access_key    = "${var.aws_access_key}"
-    secret_key    = "${var.aws_secret_key}"
+    access_key    = "${aws_iam_access_key.vault_user_access_key.id}"
+    secret_key    = "${aws_iam_access_key.vault_user_access_key.secret}"
     kms_key_id    = "${var.aws_kms_key_id}"
     kms_key_alias = "${var.aws_kms_key_alias}"
   }
 }
+
+#access_key=${aws_iam_access_key.vault_user_access_key.id}
+#secret_key=${aws_iam_access_key.vault_user_access_key.secret}
 
 output "vault_config" {
   description = "JSON configuration file for vault"
