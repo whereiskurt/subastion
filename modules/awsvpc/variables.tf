ba49919f197e112f74e7a21c8468b1e97da116af
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-20 01:25:44 -0500

    -Removing count and list(string)

--- modules/awsvpc/variables.tf
+++ modules/awsvpc/variables.tf
@@ -14,8 +14,8 @@ variable vpc_cidr {
 }
 
 variable public_subnets {
-    type=list(string)
-    default = ["10.50.0.0/20"]
+    type=string
+    default = "10.50.0.0/20"
 } 
 variable manage_subnets {
     default = "10.50.16.0/20"
