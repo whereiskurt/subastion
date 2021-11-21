d7099052c54acb71b1f8b36687a9ec1134362b67
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-20 20:56:35 -0500

    -Adding sleep and dev null outs

--- terraform/modules/aws/vault/vault.tf
+++ terraform/modules/aws/vault/vault.tf
@@ -67,13 +67,20 @@ resource "null_resource" "vault_start" {
   depends_on = [local_file.vault_config, null_resource.wait_for_iam]
   provisioner "local-exec" {
     command = <<-EOT
-      cd ../../docker/vault/ && docker-compose up -d && sleep 3
+      cd ../../docker/vault/ && docker-compose up -d
     EOT
   }
 }
 
-resource "null_resource" "vault_init" {
+resource "null_resource" "wait_for_vault" {
   depends_on = [null_resource.vault_start]
+  provisioner "local-exec" {
+    command = "sleep 5"
+  }
+}
+
+resource "null_resource" "vault_init" {
+  depends_on = [null_resource.wait_for_vault]
   provisioner "local-exec" {
     environment = var.vault_env
     command = <<-EOT
@@ -87,9 +94,10 @@ resource "null_resource" "vault_login" {
   depends_on = [null_resource.vault_init]
   provisioner "local-exec" {
     environment = var.vault_env
+    sensitive=true
     command = <<-EOT
       tail -n1 $VAULT_SECRETS_FILE | \
-        vault login -
+        vault login -f 2>&1
     EOT
   }
 }
