a036f3dfde1d688f829106e43c4658e1bdce8e2a
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-22 17:28:21 -0500

    -Complete bluegreen subastions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 export ENVDIR=`pwd`/environment/prod
 
 subastion-init() {
@@ -8,9 +7,11 @@ subastion-init() {
 
   export VAULT_ADDR=https://localhost:8200
   export VAULT_TOKEN=$(cat $ENVDIR/vaultadmin.token)
+
   export SUBASTION_GREEN_KEYFILE=$HOME/.ssh/subastion.green.key
   export SUBASTION_GREEN_IP=$(vault read -field=ip subastion/prod_green_subastion_ec2)
   
+  export SUBASTION_BLUE_KEYFILE=$HOME/.ssh/subastion.blue.key
   export SUBASTION_BLUE_IP=$(vault read -field=ip subastion/prod_blue_subastion_ec2)
 }
 
@@ -26,18 +27,27 @@ subastion-destroy() {
   unset VAULT_TOKEN
   unset VAULT_ADDR
   unset SUBASTION_GREEN_KEYFILE
-  unset SUBASTION_IP
+  unset SUBASTION_GREEN_IP
+  unset SUBASTION_BLUE_KEYFILE
+  unset SUBASTION_BLUE_IP
 }
 
 subastion-green-ssh () { 
-  #Looked at various ways to pipe in ssh avoiding the file
-  #but wasn't able to. Even the fifo wasn't working for me.
   vault read -field=pem subastion/prod_green_subastion_ec2 | base64 -d > $SUBASTION_GREEN_KEYFILE
   chmod 400 $SUBASTION_GREEN_KEYFILE
   ssh -i $SUBASTION_GREEN_KEYFILE ubuntu@$SUBASTION_GREEN_IP
   rm -f $SUBASTION_GREEN_KEYFILE
 }
 
+subastion-blue-ssh () { 
+  vault read -field=pem subastion/prod_blue_subastion_ec2 | base64 -d > $SUBASTION_BLUE_KEYFILE
+  chmod 400 $SUBASTION_BLUE_KEYFILE
+  ssh -i $SUBASTION_BLUE_KEYFILE ubuntu@$SUBASTION_BLUE_IP
+  rm -f $SUBASTION_BLUE_KEYFILE
+}
+
+
 export -f subastion-init
+export -f subastion-green-ssh
+export -f subastion-blue-ssh
 export -f subastion-destroy
-export -f subastion-ssh
