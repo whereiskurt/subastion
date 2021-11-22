4be20b2c957a50237b0bd159ee00e96a64252ff0
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-22 00:29:17 -0500

    -Updating to green/blue

--- environment/prod/outputs.tf
+++ environment/prod/outputs.tf
@@ -1,4 +1,9 @@
-output "subastion_public_ip" {
-  description = "subastion private key"
-  value       = "${module.awsbastion.subastion_public_ip}"
+output "subastion_green_public_ip" {
+  description = "subastion green public internet address"
+  value       = "${module.ec2_bastion_green.subastion_public_ip}"
+}
+
+output "subastion_blue_public_ip" {
+  description = "subastion blue public internet address"
+  value       = "${module.ec2_bastion_blue.subastion_public_ip}"
 }
