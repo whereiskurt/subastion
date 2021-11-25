74fc379de7bd1bb684055629e503d97b971bbf64
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-24 19:06:14 -0500

    -Cert variables

--- terraform/modules/aws/bastion/outputs.tf
+++ terraform/modules/aws/bastion/outputs.tf
@@ -4,7 +4,6 @@ output "subastion_public_ip" {
 }
 
 data "template_file" "bastion_boot" {
-  description="The interpolated boot_template is executed on EC2 host creation."
   template = "${file(var.boot_template)}"
   vars = {
     name = "${var.name}"
