ea3558088f1bb8f743ce5a49c34bb129eb3b44a6
Kurt Hundeck <whereiskurt@gmail.com>
2021-11-27 21:21:50 -0500

    -Updatind ../ based on module -Adding openvpn certificate build

--- environment/aws/bluegreen/variables.tf
+++ environment/aws/bluegreen/variables.tf
@@ -41,14 +41,12 @@ variable "openssl_env" {
     CA_CONF = "../../../terraform/modules/openssl/ca/ca.openssl.conf"
     CA_TPL = "../../../terraform/modules/openssl/ca/ca.openssl.tpl"
     CA_DIR = "../../../terraform/modules/openssl/ca/"
+    CA_KEY_FILE = "../../../terraform/modules/openssl/ca/ca.key.pem"    
+    CA_CERT_FILE = "../../../terraform/modules/openssl/ca/ca.cert.pem"
     
     ICA_CONF = "../../../terraform/modules/openssl/ica/ica.openssl.conf"
     ICA_TPL = "../../../terraform/modules/openssl/ica/ica.openssl.tpl"
     ICA_DIR= "../../../terraform/modules/openssl/ica/"
-    
-    CA_KEY_FILE = "../../../terraform/modules/openssl/ca/ca.key.pem"    
-    CA_CERT_FILE = "../../../terraform/modules/openssl/ca/ca.cert.pem"
-
     ICA_KEY_FILE = "../../../terraform/modules/openssl/ica/ica.key.pem"
     ICA_CSR_FILE = "../../../terraform/modules/openssl/ica/ica.csr.pem"
     ICA_CERT_FILE = "../../../terraform/modules/openssl/ica/ica.cert.pem"
--- terraform/modules/aws/bastion/variables.tf
+++ terraform/modules/aws/bastion/variables.tf
@@ -10,15 +10,15 @@ variable name {
 variable "openssl_env" {
   type = map
   default = {
-    CA_DIR = "../../../../terraform/modules/openssl/ca/"
-    CA_TPL = "../../../../terraform/modules/openssl/ca/ca.openssl.tpl"
     CA_CONF = "../../../../terraform/modules/openssl/ca/ca.openssl.conf"
+    CA_TPL = "../../../../terraform/modules/openssl/ca/ca.openssl.tpl"
+    CA_DIR = "../../../../terraform/modules/openssl/ca/"
     CA_KEY_FILE = "../../../../terraform/modules/openssl/ca/ca.key.pem"    
     CA_CERT_FILE = "../../../../terraform/modules/openssl/ca/ca.cert.pem"
     
-    ICA_DIR= "../../../../terraform/modules/openssl/ica/"
-    ICA_TPL = "../../../../terraform/modules/openssl/ica/ica.openssl.tpl"
     ICA_CONF = "../../../../terraform/modules/openssl/ica/ica.openssl.conf"
+    ICA_TPL = "../../../../terraform/modules/openssl/ica/ica.openssl.tpl"
+    ICA_DIR= "../../../../terraform/modules/openssl/ica/"
     ICA_KEY_FILE = "../../../../terraform/modules/openssl/ica/ica.key.pem"
     ICA_CSR_FILE = "../../../../terraform/modules/openssl/ica/ica.csr.pem"
     ICA_CERT_FILE = "../../../../terraform/modules/openssl/ica/ica.cert.pem"
