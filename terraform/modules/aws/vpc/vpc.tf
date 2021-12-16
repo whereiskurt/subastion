4183336df68b315cf7f10e7d341f023a5a4cda1e
Kurt Hundeck <whereiskurt@gmail.com>
2021-12-15 23:11:28 -0500

    -Fixing UDP openvpn egress rule

--- terraform/modules/aws/vpc/vpc.tf
+++ terraform/modules/aws/vpc/vpc.tf
@@ -102,4 +102,14 @@ resource "aws_default_network_acl" "default" {
     to_port    = var.openvpn_port
   } 
 
+  ##TODO: Figure out why this outbound rule is needed to support openvpn
+  egress {
+    protocol   = "udp"
+    rule_no    = 246
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  } 
+
 }
